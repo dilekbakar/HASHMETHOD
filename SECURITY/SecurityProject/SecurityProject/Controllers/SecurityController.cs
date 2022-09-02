95f4182988de750f65947b457f7908c5cef0d501
Dilek BAKAR <108486801+dilekbakar@users.noreply.github.com>
2022-09-02 10:56:50 +0300

    first commit
    first commit

---
+++ SECURITY/SecurityProject/SecurityProject/Controllers/SecurityController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace SecurityProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecurityController : ControllerBase
+    {
+        private readonly IDataProtector _dataProtector;
+        public SecurityController(IDataProtectionProvider dataprovider)
+        {
+            this._dataProtector = dataprovider.CreateProtector("This is the secret key to encrypt and decrypt");
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string plainText = "Hash Data";
+            string encryptText = this._dataProtector.Protect(plainText);
+            string decryptText = this._dataProtector.Unprotect(encryptText);
+            return Ok(new { plainText, encryptText, decryptText });
+
+
+        }
+    }
+}
